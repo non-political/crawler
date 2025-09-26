28208f19e20125813ec9f248da289407a114ce1f
earthtraveller1 <liangyuezhao7@gmail.com>
2025-09-26 11:45:41 -0400

    Implemented a very basic crawler with a set of seed URLs.

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "github.com/non-political/crawler/internal"
+import ( 
+	"os"
+	"fmt"
+	"strings"
+
+	"github.com/non-political/crawler/internal" 
+)
 
 func main() {
-	internal.Dummy()
+	seedListBytes, err := os.ReadFile("seeds.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[ERROR]: Failed to load the seeds list: %v\n", err)
+		os.Exit(-1)
+	}
+
+	seedList := string(seedListBytes)
+	for seed := range strings.Lines(seedList) {
+		urls, err := internal.ScrapePage(strings.TrimSpace(seed))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[ERROR]: %v\n", err)
+		}
+
+		for _, url := range urls {
+			fmt.Printf("Found: %s\n", url)
+		}
+	}
 }
