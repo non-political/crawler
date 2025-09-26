d9d3d5badf5eca20ce0f483074bf17319e3fc2e1
earthtraveller1 <liangyuezhao7@gmail.com>
2025-09-26 11:35:28 -0400

    Implemented a basic scraping function.

--- internal/crawler.go
+++ internal/crawler.go
@@ -17,3 +17,38 @@ func GetPageHTML(url string) (pageDom *html.Node, errReturned error) {
 	pageDom, errReturned = html.Parse(response.Body)
 	return
 }
+
+func GetPageURLs(page *html.Node) []string {
+	urls := make([]string, 0)
+
+	for element := range page.Descendants() {
+		for _, attr := range element.Attr {
+			if attr.Key == "href" {
+				urls = append(urls, attr.Val)
+			}
+		}
+	}
+
+	return urls
+}
+
+func ScrapePage(pageURL string) (foundURLs []string, err error) {
+	page, err := GetPageHTML(pageURL)
+	if err != nil {
+		return
+	}
+
+	currentURLs := GetPageURLs(page)
+
+	// Also, right now the scraping is done recursively which may cause some problems...
+	for _, url := range currentURLs {
+		foundURLs = append(foundURLs, url)
+
+		// We should probably not ignore the error, but... not sure what to do
+		// if it fails so...
+		innerURLs, _ := ScrapePage(url)
+		foundURLs = append(foundURLs, innerURLs...)
+	}
+
+	return 
+}
